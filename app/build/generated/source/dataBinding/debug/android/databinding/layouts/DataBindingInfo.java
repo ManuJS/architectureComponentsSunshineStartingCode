ce93a2c9a6028de1da70caa59f2d1a91c69298a7
cs-emanuelle-menali <emanuelle.menali@concrete.com.br>
2017-09-22 20:02:39 -0300

    Initial commit

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="c60c03f4-bdcf-4e34-9f2e-e0fbf6c54e5f")
+public class DataBindingInfo {}
